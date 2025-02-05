cebec72291908b4101d9ba8f86660bcc7977b29f
quochieng <aiyanarich64@gmail.com>
2025-02-05 13:29:03 +0300

    feat:just created server

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"forum/internals/handlers"
+)
+
+func main() {
+	if len(os.Args) > 1 {
+		fmt.Println("Too much arguments")
+		return
+	}
+
+	http.HandleFunc("/", handlers.Homepage)
+	fmt.Println("Server listen on : http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
