b2df1b46f3c02e52e5aa3648000a9fa8f3c5f119
SidneyOps75 <johnops75@gmail.com>
2025-02-12 17:10:25 +0300

    feat: Added a fuction to fetch the userId used to be diplayed in the homepage

---
+++ internals/auth/auth.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"database/sql"
+	"errors"
+	"forum/internals/database"
+)
+
+// GetUserNameByID retrieves the username by user ID
+func GetUserNameByID(userID int64) (string, error) {
+	var username string
+	err := database.DB.QueryRow("SELECT username FROM users WHERE id = ?", userID).Scan(&username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return "", errors.New("user not found")
+		}
+		return "", err
+	}
+	return username, nil
+}
